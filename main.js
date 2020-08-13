7abde22ecb80543145d5d5d804a7303e96492975
4za4s <43810079+4za4s@users.noreply.github.com>
2020-08-13 18:53:22 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,516 @@
+//
+
+//All the classes stored as an object
+var Classes = {
+    MonA: {
+        Homeroom: {
+            Class: "Homeroom",
+            From: "8:50",
+            Until: "9:08"
+        },
+        Period1: {
+            Class: "",
+            From: "9:10",
+            Until: "10:00"
+        },
+        Period2: {
+            Class: "",
+            From: "10:00",
+            Until: "10:50"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:50",
+            Until: "11:15"
+        },
+        Period3: {
+            Class: "",
+            From: "11:15",
+            Until: "12:05"
+        },
+        Period4: {
+            Class: "",
+            From: "12:05",
+            Until: "12:55"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "12:55",
+            Until: "13:40"
+        },
+        Period5: {
+            Class: "",
+            From: "13:40",
+            Until: "14:30"
+        },
+        Period6: {
+            Class: "",
+            From: "14:30",
+            Until: "15:20"
+        }
+    },
+    TueA: {
+        Period1: {
+            Class: "",
+            From: "8:50",
+            Until: "9:36"
+        },
+        Period2: {
+            Class: "",
+            From: "9:36",
+            Until: "10:22"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:22",
+            Until: "10:51"
+        },
+        Homeroom: {
+            Class: "Pastrol",
+            From: "10:51",
+            Until: "11:31"
+        },
+        Period3: {
+            Class: "",
+            From: "11:31",
+            Until: "12:17"
+        },
+        Period4: {
+            Class: "",
+            From: "12:17",
+            Until: "13:03"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "13:03",
+            Until: "13:48"
+        },
+        Period5: {
+            Class: "",
+            From: "13:48",
+            Until: "14:34"
+        },
+        Period6: {
+            Class: "",
+            From: "14:34",
+            Until: "15:20"
+        }
+    },
+    WedA: {
+        Homeroom: {
+            Class: "Homeroom",
+            From: "8:50",
+            Until: "9:08"
+        },
+        Period1: {
+            Class: "",
+            From: "9:10",
+            Until: "10:00"
+        },
+        Period2: {
+            Class: "",
+            From: "10:00",
+            Until: "10:50"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:50",
+            Until: "11:15"
+        },
+        Period3: {
+            Class: "",
+            From: "11:15",
+            Until: "12:05"
+        },
+        Period4: {
+            Class: "",
+            From: "12:05",
+            Until: "12:55"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "12:55",
+            Until: "13:40"
+        },
+        Period5: {
+            Class: "",
+            From: "13:40",
+            Until: "14:30"
+        },
+        Period6: {
+            Class: "",
+            From: "14:30",
+            Until: "15:20"
+        }
+    },
+    ThuA: {
+        Homeroom: {
+            Class: "Homeroom",
+            From: "8:50",
+            Until: "9:08"
+        },
+        Period1: {
+            Class: "",
+            From: "9:10",
+            Until: "10:00"
+        },
+        Period2: {
+            Class: "",
+            From: "10:00",
+            Until: "10:50"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:50",
+            Until: "11:15"
+        },
+        Period3: {
+            Class: "",
+            From: "11:15",
+            Until: "12:05"
+        },
+        Period4: {
+            Class: "",
+            From: "12:05",
+            Until: "12:55"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "12:55",
+            Until: "13:40"
+        },
+        Period5: {
+            Class: "",
+            From: "13:40",
+            Until: "14:30"
+        },
+        Period6: {
+            Class: "",
+            From: "14:30",
+            Until: "15:20"
+        }
+    },
+    FriA: {
+        Homeroom: {
+            Class: "Homeroom",
+            From: "8:50",
+            Until: "9:08"
+        },
+        Period1: {
+            Class: "",
+            From: "9:10",
+            Until: "10:00"
+        },
+        Period2: {
+            Class: "",
+            From: "10:00",
+            Until: "10:50"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:50",
+            Until: "11:15"
+        },
+        Period3: {
+            Class: "",
+            From: "11:15",
+            Until: "12:05"
+        },
+        Period4: {
+            Class: "",
+            From: "12:05",
+            Until: "12:55"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "12:55",
+            Until: "13:40"
+        },
+        Period5: {
+            Class: "",
+            From: "13:40",
+            Until: "14:30"
+        },
+        Period6: {
+            Class: "",
+            From: "14:30",
+            Until: "15:20"
+        }
+    },
+    MonB: {
+        Homeroom: {
+            Class: "Homeroom",
+            From: "8:50",
+            Until: "9:08"
+        },
+        Period1: {
+            Class: "",
+            From: "9:10",
+            Until: "10:00"
+        },
+        Period2: {
+            Class: "",
+            From: "10:00",
+            Until: "10:50"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:50",
+            Until: "11:15"
+        },
+        Period3: {
+            Class: "",
+            From: "11:15",
+            Until: "12:05"
+        },
+        Period4: {
+            Class: "",
+            From: "12:05",
+            Until: "12:55"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "12:55",
+            Until: "13:40"
+        },
+        Period5: {
+            Class: "",
+            From: "13:40",
+            Until: "14:30"
+        },
+        Period6: {
+            Class: "",
+            From: "14:30",
+            Until: "15:20"
+        }
+    },
+    TueB: {
+        Period1: {
+            Class: "",
+            From: "8:50",
+            Until: "9:36"
+        },
+        Period2: {
+            Class: "",
+            From: "9:36",
+            Until: "10:22"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:22",
+            Until: "10:51"
+        },
+        Homeroom: {
+            Class: "Pastrol",
+            From: "10:51",
+            Until: "11:31"
+        },
+        Period3: {
+            Class: "",
+            From: "11:31",
+            Until: "12:17"
+        },
+        Period4: {
+            Class: "",
+            From: "12:17",
+            Until: "13:03"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "13:03",
+            Until: "13:48"
+        },
+        Period5: {
+            Class: "",
+            From: "13:48",
+            Until: "14:34"
+        },
+        Period6: {
+            Class: "",
+            From: "14:34",
+            Until: "15:20"
+        }
+    },
+    WedB: {
+        Homeroom: {
+            Class: "Homeroom",
+            From: "8:50",
+            Until: "9:08"
+        },
+        Period1: {
+            Class: "",
+            From: "9:10",
+            Until: "10:00"
+        },
+        Period2: {
+            Class: "",
+            From: "10:00",
+            Until: "10:50"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:50",
+            Until: "11:15"
+        },
+        Period3: {
+            Class: "",
+            From: "11:15",
+            Until: "12:05"
+        },
+        Period4: {
+            Class: "",
+            From: "12:05",
+            Until: "12:55"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "12:55",
+            Until: "13:40"
+        },
+        Period5: {
+            Class: "",
+            From: "13:40",
+            Until: "14:30"
+        },
+        Period6: {
+            Class: "",
+            From: "14:30",
+            Until: "15:20"
+        }
+    },
+    ThuB: {
+        Homeroom: {
+            Class: "Homeroom",
+            From: "8:50",
+            Until: "9:08"
+        },
+        Period1: {
+            Class: "",
+            From: "9:10",
+            Until: "10:00"
+        },
+        Period2: {
+            Class: "",
+            From: "10:00",
+            Until: "10:50"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:50",
+            Until: "11:15"
+        },
+        Period3: {
+            Class: "",
+            From: "11:15",
+            Until: "12:05"
+        },
+        Period4: {
+            Class: "",
+            From: "12:05",
+            Until: "12:55"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "12:55",
+            Until: "13:40"
+        },
+        Period5: {
+            Class: "",
+            From: "13:40",
+            Until: "14:30"
+        },
+        Period6: {
+            Class: "",
+            From: "14:30",
+            Until: "15:20"
+        }
+    },
+    FriB: {
+        Homeroom: {
+            Class: "Homeroom",
+            From: "8:50",
+            Until: "9:08"
+        },
+        Period1: {
+            Class: "",
+            From: "9:10",
+            Until: "10:00"
+        },
+        Period2: {
+            Class: "",
+            From: "10:00",
+            Until: "10:50"
+        },
+        Recess: {
+            Class: "Recess",
+            From: "10:50",
+            Until: "11:15"
+        },
+        Period3: {
+            Class: "",
+            From: "11:15",
+            Until: "12:05"
+        },
+        Period4: {
+            Class: "",
+            From: "12:05",
+            Until: "12:55"
+        },
+        Lunch: {
+            Class: "Lunch",
+            From: "12:55",
+            Until: "13:40"
+        },
+        Period5: {
+            Class: "",
+            From: "13:40",
+            Until: "14:30"
+        },
+        Period6: {
+            Class: "",
+            From: "14:30",
+            Until: "15:20"
+        }
+    },
+}
+function getTime() {
+    var time = new Date() 
+    time = time.getHours( )+ ":" +  time.getMinutes();
+    document.getElementById("Time").innerHTML = time;
+}
+
+function displayTime() {
+    getTime()
+    mytime = setInterval('getTime()', 20000)
+}
+
+function Save() {
+    for(var days in Classes){
+        period = 1
+        for(var periods in Classes[days]){
+            if(!(periods == "Homeroom" || periods == "Lunch" || periods == "Recess")){
+                Classes[days][periods].Class = document.getElementById(days + period).value
+                period += 1;
+            }
+        }
+    }
+
+    var Save = JSON.stringify(Classes)
+    localStorage.setItem("Save", Save)
+}
+
+function Load() {
+    if(localStorage.getItem("Save") !== null){
+        Classes = JSON.parse(localStorage.getItem("Save"))
+
+        for(var days in Classes){
+            period = 1
+            for(var periods in Classes[days]){
+                if(!(periods == "Homeroom" || periods == "Lunch" || periods == "Recess")){
+                    document.getElementById(days + period).value = Classes[days][periods].Class
+                    period += 1;
+                }
+            }
+        }
+    }
+}
