863afca893664f59645f80e980bcc9d2da52dd3e
4za4s <43810079+4za4s@users.noreply.github.com>
2020-08-17 09:55:19 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -489,7 +489,7 @@ function makeMinutes(time)
 }
 
 function getClass() {
-    var time = new Date("08/10/2020 8:50")
+    var time = new Date()
     x = (time.getTime() - firstDay.getTime()) / (1000 * 3600 * 24)
     x = Math.floor(x % 14)
 
