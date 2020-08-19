e03bf91dad6e09f9dc0db975d2404c60222f799f
4za4s <43810079+4za4s@users.noreply.github.com>
2020-08-19 13:35:06 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -624,6 +624,5 @@ function Load() {
             var Volume = JSON.stringify(volumeslider.value)
             localStorage.setItem("Volume", Volume)
         } )
-        window.alert("This site will play audio, mute audio if you don't want it to")
     }
 }
